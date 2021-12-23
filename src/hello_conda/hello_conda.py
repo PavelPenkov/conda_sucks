4debcce71895b7e6aba47d272f6196f187453e35
Pavel Penkov <pavel.penkov@aliexpress.ru>
2021-12-23 22:49:31 +0300

    Add Spark

--- src/hello_conda/hello_conda.py
+++ src/hello_conda/hello_conda.py
@@ -1,4 +1,5 @@
 import requests
+from pyspark.sql import SparkSession
 
 def fuck():
     print("Fuck Conda!")
@@ -6,3 +7,9 @@ def fuck():
 def request():
     r = requests.get("https://google.com")
     print(r.text)
+
+def spark():
+    spark = SparkSession.builder.getOrCreate()
+    df = spark.createDataFrame([(1, 1.0), (2, 2.0)], ("id", "value"))
+    df.write.format("parquet").mode("overwrite").save("/tmp/with_conda")
+    request()
